a0254d4712fe8d4756e4307af50cb383b25ce291
AndOne1shot <wnduddud97@naver.com>
2024-12-31 12:30:00 +0900

    버튼 추가

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { useState } from 'react';
 function App() {
 
   let post = '포켓몬';
-  let [글제목,b] =  useState('포켓몬 추천'); // state : 변동시 자동으로 html에 반영되게 만들고 싶을 때 사용
+  let [스타팅,b] =  useState(['파이리','꼬부기','이상해씨']); // state : 변동시 자동으로 html에 반영되게 만들고 싶을 때 사용
+  let [따봉,따봉변경] = useState(0);
+
 
   return (
     <div className="App">
@@ -16,8 +18,18 @@ function App() {
       <h4 style={{fontSize: '30px'}}>{post}</h4>
 
       <div className="list">
-          <h4>{글제목}</h4>
+          <h4>{스타팅[0]} <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
           <p>불꽃타입</p>
+      </div>
+
+      <div className="list">
+          <h4>{스타팅[1]}</h4>
+          <p>물타입</p>
+      </div>
+
+      <div className="list">
+          <h4>{스타팅[2]}</h4>
+          <p>풀타입</p>
       </div>
     </div>
   );
