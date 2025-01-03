0fc75f800f3e07ba6cb873efc858fb4e6d4fb03d
AndOne1shot <wnduddud97@naver.com>
2025-01-03 12:24:11 +0900

    router를 사용한 페이지 이동

--- src/App.js
+++ src/App.js
@@ -1,54 +1,68 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import { useState } from 'react';
+import FirePokemon from './pages/FirePokemon';
 
 function App() {
-
   let post = '포켓몬';
-  let [스타팅,스타팅변경] =  useState(['파이리','꼬부기','이상해씨']); // state : 변동시 자동으로 html에 반영되게 만들고 싶을 때 사용
-  let [따봉,따봉변경] = useState(0);
-
+  let [스타팅, 스타팅변경] = useState(['파이리', '꼬부기', '이상해씨']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
-      <div className="black-nav">
-        <h4 style={{color : 'purple', fontSize: '40px'}}>React practice</h4>
-      </div>
+      <BrowserRouter>
+        <div className="black-nav">
+          <h4 style={{color : 'purple', fontSize: '40px'}}>React practice</h4>
+        </div>
+        
+        <h4 style={{fontSize: '30px'}}>{post}</h4>
+
       
-      <h4 style={{fontSize: '30px'}}>{post}</h4>
 
+        <button onClick={() => {
+          let copy = [...스타팅];
+          copy[0] = '브케인';
+          스타팅변경(copy);
+        }}>
+          수정
+        </button>
 
-      <button onClick={()=>{
-        let copy1 = [...스타팅];
-        copy1.sort();
-        스타팅변경(copy1);
-      }}>
-        가나다순 정렬
-      </button>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Link to="/fire-pokemon">
+                <div className="list">
+                  <h4>{스타팅[0]} <span onClick={(e) => { e.preventDefault(); 따봉변경(따봉 + 1); }}>👍</span> {따봉} </h4>
+                  <p>불꽃타입</p>
+                </div>
+              </Link>
+              
+              <div className="list">
+                <h4>{스타팅[1]}</h4>
+                <p>물타입</p>
+              </div>
 
+              <div className="list">
+                <h4>{스타팅[2]}</h4>
+                <p>풀타입</p>
+              </div>
 
-      <button onClick={()=>{
-        let copy = [...스타팅];   // state 가 array/object면 독립적 카피본을 만들어서 수정해야 한다. -> ...의 역할은 []를 벗겨주는 역할
-        copy[0] = '브케인';
-        스타팅변경(copy);
-      }}>
-      수정
-      </button>
+              <Modal />
+            </>
+          } />
+          <Route path="/fire-pokemon" element={<FirePokemon />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
 
-      <div className="list">
-          <h4>{스타팅[0]} <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
-          <p>불꽃타입</p>
-      </div>
-
-      <div className="list">
-          <h4>{스타팅[1]}</h4>
-          <p>물타입</p>
-      </div>
-
-      <div className="list">
-          <h4>{스타팅[2]}</h4>
-          <p>풀타입</p>
-      </div>
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>이름</h4>
+      <p>타입</p>
+      <p>상세내용</p>
     </div>
   );
 }
---
+++ src/pages/FirePokemon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FirePokemon() {
+  return (
+    <div>
+      <h2>불꽃타입 포켓몬 상세 정보</h2>
+      <p>이 페이지는 불꽃타입 포켓몬에 대한 상세 정보를 포함합니다.</p>
+    </div>
+  );
+}
+
+export default FirePokemon;
